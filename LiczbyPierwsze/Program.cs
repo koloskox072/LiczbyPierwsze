6d95db8da569ea3a5cddfe73a5face6e3f337284
2m <2m@S213-05>
2023-10-31 13:44:50 +0100

    Dodaj pliki projektów.

---
+++ LiczbyPierwsze/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LiczbyPierwsze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int x = 191;
+            bool czyPierwsza = true;
+            for (int i = 2; i < x / 2; i++)
+                if (i % 2 == 0) { czyPierwsza = false; break; } 
+            if(czyPierwsza) { Console.WriteLine("Liczba Pierwsza"); }
+            else { Console.WriteLine("Liczba Nie pierwsza"); }
+            
+        }
+    }
+}
